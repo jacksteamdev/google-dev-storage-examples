07dd755ee25f915804d0bf4786ce3507cd83bd8e
Jack Steam <jacksteamdev@gmail.com>
2022-09-11 10:32:45 -0500

    convert options example to top level await

--- options.js
+++ options.js
@@ -1,14 +1,19 @@
+// extension page modules can use top level await (TLA)
+// TLA is not available for module type service workers
+// TODO: add top level await MDN link
+export {};
+
 // In-page cache of the user's options
 const options = {};
+const optionsForm = document.getElementById("optionsForm");
 
 // Initialize the form with the user's option settings
-chrome.storage.sync.get('options', (data) => {
-  Object.assign(options, data.options);
-  optionsForm.debug.checked = Boolean(options.debug);
-});
+const data = await chrome.storage.sync.get("options");
+Object.assign(options, data.options);
+optionsForm.debug.checked = Boolean(options.debug);
 
 // Immediately persist options changes
-optionsForm.debug.addEventListener('change', (event) => {
+optionsForm.debug.addEventListener("change", (event) => {
   options.debug = event.target.checked;
-  chrome.storage.sync.set({options});
+  chrome.storage.sync.set({ options });
 });
